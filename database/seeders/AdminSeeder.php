682ff5e22d00d0769358770e7759b005996e2fa4
SupunMA <supunmadhusanka62@gmail.com>
2023-02-02 10:38:32 +0530

    Added Admin Seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+//Used DB and Hash Classes
+use DB;
+use Hash;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('12345678'),
+            'role' => '1'
+        ]);
+    }
+}
